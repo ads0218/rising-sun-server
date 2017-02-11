af5380f38f3fcc6482f089ca4d36fe17cccd6561
bdh1011 <bdh931101@gmail.com>
2017-02-11 11:14:01 +0000

    Update route url

--- app_server/controllers/realestate/rest.py
+++ app_server/controllers/realestate/rest.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app_server.controllers.realestate.api_realestate import HomeItemList, StoreItemList, GroundItemList
 from flask_restful import Api
 
-bp_realestate = Blueprint('realestate', __name__, url_prefix='/api/realestate')
+bp_realestate = Blueprint('realestate', __name__, url_prefix='/rising/api/realestate')
 realestate_rest = Api(bp_realestate)
 
 realestate_rest.add_resource(HomeItemList, '/home')
